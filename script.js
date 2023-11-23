06faf43f8f7223a5fb57e0e15d1776eaf6618fe9
Fedjaosman <fedjaosmanovic@cmail.carleton.ca>
2023-11-23 15:51:22 -0500

    html, and javascript

--- script.js
+++ script.js
@@ -1,43 +1,104 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+// Selecting elements
+const inputBox = document.getElementById("todoInput");
+const addButton = document.querySelector(".btn");
+const todoList = document.getElementById("todoList");
+const todoCount = document.querySelector(".todoCount span");
+const doneCount = document.querySelector(".doneCount span");
+const clearButton = document.querySelector(".clearBtn");
 
+// Event listener for the add button
+addButton.addEventListener("click", function(){
+  addTask();
+});
 
-//
-// Variables
-//
+// Event listener for the input box (pressing Enter)
+inputBox.addEventListener("keypress", function(e){
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+// Event for checkbox and delete button
+todoList.addEventListener("click", function(e){
+  const target = e.target;
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+  // Check if the clicked element is a checkbox
+  if (target.type === "checkbox"){
+    toggleTaskStatus(target);
   }
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+  // Check if the clicked element is the delete button
+  if (target.classList.contains("deleteTask")){
+    deleteTask(target.parentElement);
+  }
+});
 
-  // Init complete
-  console.log("App successfully initialised");
+// Event listener for the clear button
+clearButton.addEventListener("click", function(){
+  clearAllTasks();
+});
+
+// Function to add a new task///////////////////////////////////////////////
+function addTask(){
+  const taskText = inputBox.value;
+  if (taskText !== ""){
+    // Create elements
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const taskTextSpan = document.createElement("span");
+    const deleteButton = document.createElement("button");
+
+    // Set attributes and content
+    checkbox.type = "checkbox";
+    taskTextSpan.textContent = taskText;
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("deleteTask");
+
+    // Append elements to the list item
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskTextSpan);
+    listItem.appendChild(deleteButton);
+
+    // Append the list item to the todo list
+    todoList.appendChild(listItem);
+
+    inputBox.value = "";
+    updateTaskCount();
+
+  }
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+// Function to toggle the status of a task (checked or unchecked) //////////
+function toggleTaskStatus(checkbox){
+    const listItem = checkbox.parentElement;
+    listItem.classList.toggle("completed");
+
+    if (listItem.classList.contains("completed")){
+        todoList.appendChild(listItem);
+    }else{
+        todoList.prepend(listItem, todoList.firstChild);
+    }
+
+    updateTaskCount();
+}
+
+// Function to delete a task //////////////////////////////////////////////
+function deleteTask(listItem){
+  listItem.remove();
+  updateTaskCount();
+}
+
+// Function to clear all tasks ////////////////////////////////////////////
+function clearAllTasks(){
+  todoList.innerHTML = "";
+  updateTaskCount();
+}
+
+// Function to update task count //////////////////////////////////////////
+function updateTaskCount(){
+  const totalTasks = todoList.children.length;
+  const completedTasks = document.querySelectorAll(".completed").length;
+
+  todoCount.textContent = totalTasks - completedTasks;
+  doneCount.textContent = completedTasks;
+}
